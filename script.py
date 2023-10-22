64d1a87ae45d763dfe113d9fb5eaa0efe5958db8
soulaiman <elmanioui.soulaimane@gmail.com>
2023-10-22 23:48:04 +0100

    transitioning to cli tool

--- script.py
+++ script.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python3
 import os
 import json
+import argparse
 from utils import make_dir, organize_type, find_dir
 
-cwd = os.getcwd()  
-print("Current working directory:", cwd)
+def main():
 
+    target_name = input("Type in the name of the folder you want to organize: ")
+    target_path = find_dir('/', target_name)
 
-target_name = 'traditional-hat-set'
-target_path = find_dir('/', target_name)
-print(f"the path for {target_name} is => {target_path}")
+    cwd = os.getcwd()
 
-"""with open('data.json', 'r') as data:
-    files_data = json.load(data)
+    with open('data.json', 'r') as data:
+        files_data = json.load(data)
 
-    if cwd is not downloads_dir:
-        os.chdir(downloads_dir)
-        cwd = os.getcwd()
-    
-    folders_dir = make_dir('folders', cwd)
-    other_dir = make_dir('other', cwd)
+        if cwd is not target_path:
+            os.chdir(target_path)
+            cwd = os.getcwd()
+        
+        folders_dir = make_dir('folders', cwd)
+        other_dir = make_dir('other', cwd)
 
-    for element in files_data:
-        organize_type(element, cwd, folders_dir, other_dir)"""
+        for element in files_data:
+            organize_type(element, cwd, folders_dir, other_dir)
+
+if __name__ == "__main__":
+    main()
 
 
     
