fc26ef8f842a44941d9bdcbbaa29123784f321ac
soulaiman <elmanioui.soulaimane@gmail.com>
2023-11-27 23:09:47 +0100

    handling of multiple use cases + new todos

--- script.py
+++ script.py
@@ -1,45 +1,37 @@
 #!/usr/bin/env python3
 import os
+import pyinputplus as pyip
 import json
 import argparse
-from utils import organize_entry, find_dir
+from utils import organize, find_dir
 
+
+#TO DO: must isolate tasks codes i.e components, maybe put each task in a module ?
 def main():
-
-    #1-GET THE WANTED DISTINATION
-    #must validate input
-    target_name = input("Type in the name of the folder you want to organize: ")
+    target_name = pyip.inputStr("(i) Type in the name of the folder you want to organize: ")
     target_paths = find_dir(target_name)
-    target_path = None
 
-    if len(target_paths) is not 0:
+    if len(target_paths) != 0:
         if len(target_paths) > 1:
-            print(f"(i) It seems like there are multiple folders in your computer that are named \"{target_name}\".")
-            print("(i) Here are the paths for each folder that I found:")
-            counter = 0
-            for item in target_paths:
-                counter += 1
-                print(f"(i) {counter}-{item}")
-        
-            #must validate input
-            item_index = input("(i) Type the number of the wanted destination:")
-            target_path = target_paths[int(item_index)-1]
+            print(f"(i) It seems like there are multiple entries in your computer that are named \"{target_name}\".")
+            #asks the user to choose one destination by number, and returns chosen path from list
+            target_path = pyip.inputMenu(target_paths, lettered=False, numbered=True)
+            organize(target_name, target_path)
 
         elif len(target_paths) == 1:
-            target_path = target_paths[0]
-        
-        if os.getcwd() is not target_path:
-            os.chdir(target_path)
-    
-        #organizing elements only if they exist
-        if len(os.listdir()):
-            for entry in os.listdir():
-                organize_entry(entry)
-    else:
-        print(f"There's no folder named \"{target_name}\"")
-    
-    print("Done!")
+            answer = pyip.inputYesNo(f"(i) I've found one destination with the name \"{target_name}\", is this the one ({target_paths[0]}) ?")
+            if answer == "yes":
+                target_path = target_paths[0]
+                organize(target_name, target_path)
+            else:
+                #TO DO: ask the user if they want to do something else, but since he wanted to organize something in the beginning,
+                #reprompt him to give you another name
+                print("Okay, bye bye!")
 
+    else:
+        print(f"Unfortunatly I did not found any file or folder with the name \"{target_name}\"")
+        #must ask the user if they want to try again
+    
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -14,15 +14,19 @@ def organize_entry(entry):
     elif os.path.isdir(entry):
         folders_dir = make_dir('folders')
 
-        #must also check if the folder I am about to organize is not among the default user dirs
+        #TO DO: must also check if the folder I am about to organize is not among the default user dirs
         if not any(name in entry for name in DESTINATIONS): 
             destination = os.path.join(folders_dir, entry)
 
     else:
         other_dir = make_dir('other')
         destination = os.path.join(other_dir, entry)
-    
-    os.rename(source, destination)
+
+    if source != destination:
+        os.rename(source, destination)
+        return entry
+    else:
+        return None
 
 def make_dir(name):
     new_dir_path = os.path.join(os.getcwd(), name)
@@ -55,3 +59,26 @@ def getTypeByEntry(entry):
                  for extension in dict.get("extensions", [])):
                     return dict.get("type")
     return None
+
+def organize(target_name, target_path):
+    os.chdir(target_path)
+    
+    #organizing elements only if they exist
+    if len(os.listdir()):
+        organized_entries = list()
+        for entry in os.listdir():
+            organized_entry = organize_entry(entry)
+            if organize_entry is not None:
+                organized_entries.append(organized_entry)
+
+        #only display this message if there was something to be organized to begin with
+        if len(organized_entries):
+            print(f"(i) The following entries have been organized successfully!")
+            #TO DO: the following loop must be put in a function!
+            for entry in organized_entries:
+                print(f"{entry}")
+        else:
+            print(f"(i) Looks like \"{target_name}\" is already organized!")
+    else:
+        print(f"(i) Oops! \n")
+        print(f"(i) Looks like the folder you chose \"{target_name}\" is empty!")
